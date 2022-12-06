4a9f4c7cb9bf7c81100cebc8a758fac82a858a53
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2022-12-07 02:05:24 +0900

    mysqlからsqliteに変更

--- Php01/php01-use-pdo.php
+++ Php01/php01-use-pdo.php
@@ -5,23 +5,31 @@ namespace Php01;
 
 require_once 'myautoload.php';
 
-use \Php01\Classes\DbMysql;
+// use \Php01\Classes\DbMysql;
+use \Php01\Classes\DbSqlite;
 
 echo '<h1>'.__FILE__.'</h1>'."\n";
 
-$pdo = DbMysql::getPdo();
+// $pdo = DbMysql::getPdo();
+$pdo = DbSqlite::getPdo();
 
 // If DB does not exist, creating it.
+/*
 $dbName = 'php_sample';
 if ($pdo->query("SHOW DATABASES LIKE '$dbName'")->rowCount() <= 0) {
     $pdo->exec("CREATE DATABASE ".$dbName);
 }
 $pdo->exec("USE ".$dbName);
+*/
 
 // If table does not exist, creating it.
 $tableName = 'sample_values';
-if ($pdo->query("SHOW TABLES LIKE '$tableName'")->rowCount() <= 0) {
-    $sql = "CREATE TABLE ".$tableName."(id BIGINT AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(id))";
+// $selectTableName = "SHOW TABLES LIKE '$tableName'";
+$selectTableName = "SELECT name FROM sqlite_master WHERE type='table' AND name='$tableName'";
+$data = $pdo->query($selectTableName)->fetchAll();
+if (count($data) <= 0) {
+    // $sql = "CREATE TABLE ".$tableName."(id BIGINT AUTO_INCREMENT, name VARCHAR(255), PRIMARY KEY(id))";
+    $sql = "CREATE TABLE ".$tableName."(id INTEGER PRIMARY KEY, name)";
     $pdo->exec($sql);
     $names = ['Alice', 'Bob', 'Carol', 'Dave'];
     $stmt = $pdo->prepare("INSERT INTO ".$tableName."(name) values(:name)");
