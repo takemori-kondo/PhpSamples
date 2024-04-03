0d6e0978e4e42a9ab218b7565cc9013ff195c94d
HOME-20230701\Ether <etherealgarden@msn.com>
2024-04-03 17:36:13 +0900

    php改定

--- Php01/php01-syntax01-statements.php
+++ Php01/php01-syntax01-statements.php
@@ -1,6 +1,7 @@
 <?php
 // PHP Version 8.1
 declare(strict_types=1);
+
 namespace Php01;
 
 /*
@@ -25,7 +26,8 @@ namespace Php01;
 ;    ファイルエンコーディングがSJIS等でなければscript_encodingはデフォルトのISO-8859-1(Latin-1)で問題ない
 ;    ファイル中のリテラルはdefault_charset(internal_encoding)で処理されるため、問題ない
 ;    なお、ファイルエンコーディングをファイル個別に指定するdeclare(encoding=...)は、この機能を有効にしないと機能しない
-; mbstring
+; 
+: mbstring
 mbstring.detect_order = UTF-8,EUC-JP,SJIS,JIS,ASCII
 ; mbstring.language ("neutral")
 ;    この値は、具体的にはmb_encode_mimeheader、mb_send_mailの2つに影響する
@@ -40,110 +42,108 @@ mbstring.http_output = NULL
 ; mbstring.strict_detection (0)
 ; mbstring.substitute_character (NULL)
 */
-echo '<h1>'.__FILE__.'</h1>'."\n";
 
-echo '<h2>変数</h2>'."\n";
-$selector = 'dic';
-var_dump($selector);
-echo "<br>\n";
+echo '<h1>' . __FILE__ . '</h1>' . "\n";
 
-echo '<h2>連想配列</h2>'."\n";
+echo '<h2>1. 変数、配列、可変変数、定数</h2>' . "\n";
+$var = 'dic';
 $dic = ['foo' => 'Alice', 'bar' => 'Bob', 'baz' => 'Carol'];
-var_dump($dic);
-echo "<br>\n";
-
-echo '<h2>可変変数</h2>'."\n";
-var_dump(${$selector});
-echo "<br>\n";
-
-echo '<h2>定数</h2>'."\n";
 const CNST = '定数には$は付けない';
+echo "<pre>\n";
+var_dump($var);
+var_dump($dic);
+var_dump(${$var});
 var_dump(CNST);
-echo "<br>\n";
+echo "</pre>\n";
 
-echo '<h2>文字列リテラル3種</h2>'."\n";
-$val = '$valの中身';
-$simple = 'シングルクォートは変数展開されない:{$val}';
-$interpolation = "ダブルクォートは変数展開される:{$val}";
-$here = <<<"HEREDOC"
+echo '<h2>2. 文字列リテラル3種</h2>' . "\n";
+$val = '変数の中身';
+$single = '\'は変数展開しない:{$val}';
+$double = "\"は変数展開される:{$val}";
+$heredoc = <<<"HEREDOC"
 これはヒアドキュメントです
-お作法として、ヒアドキュメントIDは必ず"か'で囲んだほうが分かりやすいです。:{$val}
+お作法として、ヒアドキュメントIDは必ず'か"で囲んだほうが分かりやすいです。:{$val}
 HEREDOC;
-echo '<pre>'."\n";
-echo $simple."\n";
-echo $interpolation."\n";
-echo $here."\n";
-echo '</pre>'."\n";
+echo "<pre>\n";
+echo $single . "\n";
+echo $double . "\n";
+echo $heredoc . "\n";
+echo "</pre>\n";
 
-echo '<h2>厳密な比較演算</h2>'."\n";
+echo '<h2>3. 厳密な比較演算</h2>' . "\n";
 $strVar = '2';
 $numVar = 2;
-$result = $strVar === $numVar;
+echo "<pre>\n";
 var_dump($strVar);
-echo "<br>\n";
 var_dump($numVar);
-echo "<br>\n";
-var_dump($result);
-echo "<br>\n";
+var_dump($strVar == $numVar);
+var_dump($strVar === $numVar);
+echo "</pre>\n";
 
-echo '<h2>制御文</h2>'."\n";
-if (array_key_exists('baz', $dic)) {
-    echo 'if statement is TRUE!'."<br>\n";
-}
-if (array_key_exists('qux', $dic)) {
+echo '<h2>4. 制御文</h2>' . "\n";
+$cnt = 0;
+echo "<pre>\n";
+if (true) {
+    echo 'ifブロックに来ました。' . "\n";
 } else {
-    echo 'if statement is FALSE!'."<br>\n";
+    echo 'ここは通りません。' . "\n";
 }
-echo "<br>\n";
-
-while (count($dic) < 5) {
-    $x = 'This element was added when count() is '.((string)count($dic));
-    array_push($dic, $x);
+if (false) {
+    echo 'ここは通りません。' . "\n";
+} else {
+    echo 'elseブロックに来ました。' . "\n";
 }
-echo '<pre>'."\n";
-var_dump($dic);
-echo '</pre>'."\n";
-
+while ($cnt < 5) {
+    echo 'count up!' . "\n";
+    $cnt++;
+}
 foreach ($dic as $key => $val) {
-    echo $key."'s value is ".$val."<br>\n";
+    echo "key=$key, val=$val" . "\n";
 }
+echo "</pre>\n";
 
-echo '<h2>関数、配列、インスタンス</h2>'."\n";
-echo '配列は値型ライク、配列とインスタンスの変換は生成される'."\n";
-function echoCompare(mixed $arg1, mixed $arg2, string $text) : void {
-    $isEqual = ($arg1 === $arg2);
-    echo "<strong>{$text}</strong>"."<br>\n";
-    var_dump($arg1);
-    echo "<br>\n";
-    var_dump($arg2);
-    echo "<br>\n";
-    var_dump($isEqual);
-    echo "<br>\n";
+echo '<h2>5. 関数、配列、インスタンス</h2>' . "\n";
+function echoAndCompare(mixed $arg1, mixed $arg2, string $text): void
+{
+    echo "$text : ";
+    var_dump($arg1 === $arg2);
 }
+echo "<pre>\n";
+echo '配列は各要素で比較される（初期状態と最終状態の$ary1 === $ary3 が true）' . "\n";
+echo '配列からオブジェクトへのキャストは生成される（$obj1 === $obj2 が false）' . "\n";
+echo '最初の状態' . "\n";
 $ary1 = ['key1' => 'val1', 'key2' => 'val2'];
 $ary2 = $ary1;
 $obj1 = (object)$ary1;
 $obj2 = (object)$ary1;
 $ary3 = (array)$obj1;
 $obj3 = $obj1;
-echo '<h3>最初の状態</h3>'."\n";
-echoCompare($ary1, $ary2, '$ary1 === $ary2'); // true
-echoCompare($ary1, $ary3, '$ary1 === $ary3'); // true …①
-echoCompare($ary1, $obj1, '$ary1 === $obj1'); // false
-echoCompare($obj1, $obj2, '$obj1 === $obj2'); // false
-echoCompare($obj1, $obj3, '$obj1 === $obj3'); // true
-echo '<h3>ary1, obj1 にkey3を追加</h3>'."\n";
+echoAndCompare($ary1, $ary2, '$ary1 === $ary2'); // true
+echoAndCompare($ary1, $ary3, '$ary1 === $ary3'); // true
+echoAndCompare($ary1, $obj1, '$ary1 === $obj1'); // false
+echoAndCompare($obj1, $obj2, '$obj1 === $obj2'); // false
+echoAndCompare($obj1, $obj3, '$obj1 === $obj3'); // true
+echo 'ary1, obj1 にkey3を追加' . "\n";
 $ary1['key3'] = 'val3';
 $obj1->key3 = 'val3';
-echoCompare($ary1, $ary2, '$ary1 === $ary2'); // false
-echoCompare($ary1, $ary3, '$ary1 === $ary3'); // false …②
-echoCompare($ary1, $obj1, '$ary1 === $obj1'); // false
-echoCompare($obj1, $obj2, '$obj1 === $obj2'); // false
-echoCompare($obj1, $obj3, '$obj1 === $obj3'); // true
-echo '<h2>ary3に key3を追加</h2>'."\n";
+echoAndCompare($ary1, $ary2, '$ary1 === $ary2'); // false
+echoAndCompare($ary1, $ary3, '$ary1 === $ary3'); // false
+echoAndCompare($ary1, $obj1, '$ary1 === $obj1'); // false
+echoAndCompare($obj1, $obj2, '$obj1 === $obj2'); // false
+echoAndCompare($obj1, $obj3, '$obj1 === $obj3'); // true
+echo 'ary3 にkey3を追加' . "\n";
 $ary3['key3'] = 'val3';
-echoCompare($ary1, $ary2, '$ary1 === $ary2'); // false
-echoCompare($ary1, $ary3, '$ary1 === $ary3'); // true …③
-echoCompare($ary1, $obj1, '$ary1 === $obj1'); // false
-echoCompare($obj1, $obj2, '$obj1 === $obj2'); // false
-echoCompare($obj1, $obj3, '$obj1 === $obj3'); // true
+echoAndCompare($ary1, $ary2, '$ary1 === $ary2'); // false
+echoAndCompare($ary1, $ary3, '$ary1 === $ary3'); // true
+echoAndCompare($ary1, $obj1, '$ary1 === $obj1'); // false
+echoAndCompare($obj1, $obj2, '$obj1 === $obj2'); // false
+echoAndCompare($obj1, $obj3, '$obj1 === $obj3'); // true
+echo "</pre>\n";
+
+echo '<h2>6. 主な配列操作</h2>' . "\n";
+echo "<pre>\n";
+echo  var_dump($dic);
+echo 'in_array("Alice", $dic)       : ' . in_array("Alice", $dic, true) . "\n";
+echo 'array_key_exists("baz", $dic) : ' . array_key_exists("baz", $dic) . "\n";
+echo 'count($dic)                   : ' . count($dic) . "\n";
+echo "</pre>\n";
